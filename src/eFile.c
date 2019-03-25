971136f14f8d4ffc8400882d636acf4add8a95e4
Riley Wood <rjw245@gmail.com>
2019-03-25 16:54:05 -0500

    Starting on file sys implementation
    We will support 8GB of storage and up to 64 files.
    The FAT will be cached in RAM one sector at a time on demand.
    The entire directory (2 sectors, 1K) will be cached in RAM at all times.

--- src/eFile.c
+++ src/eFile.c
@@ -10,12 +10,41 @@
 #define SUCCESS 0
 #define FAIL 1
 
-typedef uint32_t block_addr_t;
-                       
+// Sector size in bytes
+#define SECTOR_BYTES 512
+
+typedef uint32_t sector_addr_t;
+
+#define LONGEST_FILENAME 7
+typedef struct {
+  char file_name[LONGEST_FILENAME + 1]; // space for null terminator
+  sector_addr_t start;
+  uint32_t padding;
+} dir_entry_t;
+
+#define DIR_START 32 // Sector number
+#define DIR_SECTORS 2 // Number of sectors directory may span
+#define DIR_ENTRIES ((DIR_SECTORS*SECTOR_BYTES)/(sizeof(dir_entry_t)))
+dir_entry_t dir[DIR_ENTRIES];
+
+#define FAT_SECTORS 131072 // Number of sectors directory may span
+#define FAT_ENTRIES ((FAT_SECTORS*SECTOR_BYTES)/sizeof(sector_addr_t))
+#define FAT_START 34
+
+sector_addr_t fat_cache[SECTOR_BYTES/sizeof(sector_addr_t)];
+sector_addr_t cached_fat_sector = 0;
 
 int eFile_Init(void)
 {
-
+  memset(dir, 0, sizeof(dir));
+  memset(fat_cache, 0, sizeof(dir));
+  eDisk_Init(0);
+  for(int i=0; i<DIR_SECTORS; i++)
+  {
+    eDisk_ReadBlock(((uint8_t*)dir)+i*SECTOR_BYTES, DIR_START+i);
+  }
+  cached_fat_sector = FAT_START;
+  eDisk_ReadBlock(fat_cache, FAT_START);
 }
 
 
