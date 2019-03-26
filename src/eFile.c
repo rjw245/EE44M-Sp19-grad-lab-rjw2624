55ea0a6bd99488fdf8d74ce61a691b0723f552fc
Riley Wood <rjw245@gmail.com>
2019-03-26 13:14:36 -0500

    Wrap dir cache/writeback, clarify comments, formatting, check for duplicate filenames, use strncpy

--- src/eFile.c
+++ src/eFile.c
@@ -46,6 +46,7 @@ static BYTE DATAarray[SECTOR_BYTES];
 static void writeback_fat_cache(void)
 {
   eDisk_WriteBlock((BYTE *)fat_cache, cached_fat_sector);
+  fat_cache_dirty = false;
 }
 
 static void cache_fat_sector(sector_addr_t abs_sector_num)
@@ -55,10 +56,25 @@ static void cache_fat_sector(sector_addr_t abs_sector_num)
     if (fat_cache_dirty)
     {
       writeback_fat_cache();
-			fat_cache_dirty = false;
     }
     cached_fat_sector = abs_sector_num;
     eDisk_ReadBlock((BYTE *)fat_cache, abs_sector_num);
+  }
+}
+
+static void writeback_dir(void)
+{
+  for (int i = 0; i < DIR_SECTORS; i++)
+  {
+    eDisk_WriteBlock(((uint8_t *)dir) + i * SECTOR_BYTES, DIR_START + i);
+  }
+}
+
+static void cache_dir(void)
+{
+  for (int i = 0; i < DIR_SECTORS; i++)
+  {
+    eDisk_ReadBlock(((uint8_t *)dir) + i * SECTOR_BYTES, DIR_START + i);
   }
 }
 
@@ -67,10 +83,7 @@ int eFile_Init(void)
   memset(dir, 0, sizeof(dir));
   memset(fat_cache, 0, sizeof(fat_cache));
   eDisk_Init(0);
-  for (int i = 0; i < DIR_SECTORS; i++)
-  {
-    eDisk_ReadBlock(((uint8_t *)dir) + i * SECTOR_BYTES, DIR_START + i);
-  }
+  cache_dir();
   cache_fat_sector(FAT_START);
   return SUCCESS;
 }
@@ -79,20 +92,19 @@ int eFile_Format(void)
 {
   memset(dir, 0, sizeof(dir));
   memset(fat_cache, 0, sizeof(fat_cache));
-  for (int i = 0; i < DIR_SECTORS; i++)
-  {
-    eDisk_WriteBlock(((uint8_t *)dir) + i * SECTOR_BYTES, DIR_START + i);
-  }
+  writeback_dir();
 
   for (int i = 0; i < FAT_SECTORS; i++)
   {
+    cached_fat_sector = FAT_START + i;
     for (int j = 0; j < CACHED_SECTORS; j++)
     {
       fat_cache[j] = i * CACHED_SECTORS + j + 1;
     }
-    eDisk_WriteBlock((BYTE *)fat_cache, FAT_START + i);
+    writeback_fat_cache();
   }
 
+  cached_fat_sector = 0;
   for (int j = 0; j < 34; j++)
   {
     fat_cache[j] = 0;
@@ -101,10 +113,8 @@ int eFile_Format(void)
   {
     fat_cache[j] = j + 1;
   }
-	cached_fat_sector = 0;
+  writeback_fat_cache();
 
-  eDisk_WriteBlock((BYTE *)fat_cache, FAT_START);
-  fat_cache_dirty = false;
   dir[0].start = 0; // sector offset from DATA_START
   dir[0].size = 1;
   return SUCCESS;
@@ -115,63 +125,67 @@ int eFile_Create(char name[])
   dir_entry_t new_file;
   int idx;
   int freespace;
-  int FATTABLE;
+  int FAT_sec_offset; // Sector offset from base sector of FAT
   for (idx = 0; idx < DIR_ENTRIES; idx++)
   {
+    if (strncmp(dir[idx].file_name, name, LONGEST_FILENAME) == 0)
+    {
+      return FAIL; // There already is a file with this name
+    }
     if (dir[idx].size == 0)
-      break;
+    {
+      break; // Free dir entry at idx, we will allocate it to this file
+    }
   }
   if (idx == DIR_ENTRIES)
   {
-    return FAIL; // FULL Files
+    return FAIL; // Directory is full, cannot create new file
   }
   strncpy(dir[idx].file_name, name, LONGEST_FILENAME);
-  // Create directory with given name.
 
-  dir[idx].size = 1;                                        // set size.
-  FATTABLE = dir[0].start / CACHED_SECTORS;                 // FATTABLE will save which sector to load from the disk
-  cache_fat_sector(FATTABLE + FAT_START); // Read sector from disk
+  // Create directory entry with given name.
+  dir[idx].size = 1; // set size to indicate entry occupied. True size of file is dir[].size - 1
+  FAT_sec_offset = dir[0].start / CACHED_SECTORS;
+  cache_fat_sector(FAT_sec_offset + FAT_START); // Read sector of FAT from disk
 
   freespace = dir[0].start;                             // get free space that we could use
   dir[0].start = fat_cache[freespace % CACHED_SECTORS]; // set head of free space to nextone. (Next freespace using linked list kind of work)
 
-  fat_cache[freespace % CACHED_SECTORS] = 0;                 // This is creating part, make sure currently allocated space is last one.
-	fat_cache_dirty = true;
-  //eDisk_WriteBlock((BYTE *)fat_cache, FATTABLE + FAT_START); // write back to the disk.
+  fat_cache[freespace % CACHED_SECTORS] = 0; // This is creating part, make sure currently allocated space is last one.
+  fat_cache_dirty = true;
+  //eDisk_WriteBlock((BYTE *)fat_cache, FAT_sec_offset + FAT_START); // write back to the disk.
   return SUCCESS;
 }
 
 int eFile_WOpen(char name[])
 {
-	int open_idx = 0;
-	int FATTABLE;
-	int iter = 0;
-	int prev_iter = 0;
-	for(int i =0;i<DIR_ENTRIES;i++)
-	{
-		if(strcmp(name,dir[i].file_name) == 0)
-		{
-			open_idx = i;
-			write_mode = true;
-			break;
-		}
-	}
-	if(open_idx ==0)
-		return FAIL;
-	FATTABLE = dir[open_idx].start / CACHED_SECTORS;
-	prev_iter = dir[open_idx].start;
-	cache_fat_sector(FATTABLE + FAT_START);
-	iter = fat_cache[prev_iter%CACHED_SECTORS];
-	while(iter!=0)
-	{
-		FATTABLE = iter / CACHED_SECTORS;
-		cache_fat_sector(FATTABLE + FAT_START);
-		prev_iter = iter;
-		iter = fat_cache[prev_iter%CACHED_SECTORS];
-	}
-	write_point_in_sector = dir[open_idx].size-1;
-	
-	
+  int open_idx = 0;
+  int FAT_sec_offset; // Sector offset from base sector of FAT
+  int iter = 0;
+  int prev_iter = 0;
+  for (int i = 0; i < DIR_ENTRIES; i++)
+  {
+    if (strcnmp(name, dir[i].file_name, LONGEST_FILENAME) == 0)
+    {
+      open_idx = i;
+      write_mode = true;
+      break;
+    }
+  }
+  if (open_idx == 0)
+    return FAIL;
+  FAT_sec_offset = dir[open_idx].start / CACHED_SECTORS;
+  prev_iter = dir[open_idx].start;
+  cache_fat_sector(FAT_sec_offset + FAT_START);
+  iter = fat_cache[prev_iter % CACHED_SECTORS];
+  while (iter != 0)
+  {
+    FAT_sec_offset = iter / CACHED_SECTORS;
+    cache_fat_sector(FAT_sec_offset + FAT_START);
+    prev_iter = iter;
+    iter = fat_cache[prev_iter % CACHED_SECTORS];
+  }
+  write_point_in_sector = dir[open_idx].size - 1;
 }
 
 int eFile_Write(char data)
