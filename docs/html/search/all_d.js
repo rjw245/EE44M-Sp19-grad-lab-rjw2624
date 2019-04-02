9bc82455b99562d95cd6c908c7995016f9dc3386
Riley Wood <rjw245@gmail.com>
2019-04-02 09:22:51 -0500

    Update documentation

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['uart_2eh',['UART.h',['../UART_8h.html',1,'']]],
+  ['uart_5fendredirect',['UART_endRedirect',['../UART_8h.html#ae3522f1847db40b20a24172b5c6f224b',1,'UART.h']]],
   ['uart_5finchar',['UART_InChar',['../UART_8h.html#a00e894bb188320a2f4dcd5a78b80da52',1,'UART.h']]],
   ['uart_5finit',['UART_Init',['../UART_8h.html#ad5cbed2a2222bb84e8b5c1caaa50634e',1,'UART.h']]],
   ['uart_5finstring',['UART_InString',['../UART_8h.html#a4278ab3463fadff60a5a84792707c3a3',1,'UART.h']]],
@@ -9,5 +10,6 @@ var searchData=
   ['uart_5foutchar',['UART_OutChar',['../UART_8h.html#a4ef2f92682b12a347cf1f81cccda4da7',1,'UART.h']]],
   ['uart_5foutstring',['UART_OutString',['../UART_8h.html#a2cbbed822dc8e6d801e6c9f21a2cd418',1,'UART.h']]],
   ['uart_5foutudec',['UART_OutUDec',['../UART_8h.html#a9a53c5fe8486e0282990b11a218c2625',1,'UART.h']]],
-  ['uart_5foutuhex',['UART_OutUHex',['../UART_8h.html#a21661aabfda94ec88e9514856f062a41',1,'UART.h']]]
+  ['uart_5foutuhex',['UART_OutUHex',['../UART_8h.html#a21661aabfda94ec88e9514856f062a41',1,'UART.h']]],
+  ['uart_5fsetredirect',['UART_setRedirect',['../UART_8h.html#a7951d2bd4596b8398c204cc7292cf668',1,'UART.h']]]
 ];
